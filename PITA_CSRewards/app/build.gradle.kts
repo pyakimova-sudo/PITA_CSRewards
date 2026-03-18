44d8cd3984fa3e2730ef8bef0757f7e75ec1d3d0
IStone0 <ianstone@gustavus.edu>
2026-03-18 18:59:22 -0500

    New branch

--- PITA_CSRewards/app/build.gradle.kts
+++ PITA_CSRewards/app/build.gradle.kts
@@ -2,16 +2,16 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    //Does this work video outdated by 3 years
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.pita_csrewards"
-    compileSdk {
-        version = release(36)
-    }
+    namespace = "com.example.loginapp"
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.pita_csrewards"
+        applicationId = "com.example.loginapp"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -29,27 +29,44 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
+        viewBinding = true
         compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3" // adjust if needed
     }
 }
 
 dependencies {
+    // AndroidX core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    // Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    //implementation(libs.material)
+    //implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.appcompat)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +74,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Firebase BoM and SDKs
+    implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
+    implementation("com.google.firebase:firebase-database")
+
 }
