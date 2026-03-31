7e119a0742d1fc1a4c8d34ca0bc4fe51805076f1
pyakimova-sudo <pyakimova@gustavus.edu>
2026-03-31 12:58:14 -0500

    Timer pop up

--- PITA_Rewards2/app/src/main/java/com/example/pita_rewards2/Account.kt
+++ PITA_Rewards2/app/src/main/java/com/example/pita_rewards2/Account.kt
@@ -1,24 +1,38 @@
 package com.example.pita_rewards2
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
+import com.example.pita_rewards2.databinding.ActivityAccountBinding
+import com.example.pita_rewards2.databinding.ActivityBasketBinding
+
 class Account : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAccountBinding
     lateinit var navigation : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAccountBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_account)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.account)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, 0, systemBars.right, 0)
             insets
+        }
+
+        binding.drinkTimer.setOnClickListener {
+            val intent = Intent(this, TimerPopUp::class.java)
+            startActivity(intent)
         }
 
         navigation =  findViewById(R.id.bottom_navigation)
