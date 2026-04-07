df87124194ca11c84736466f545af9ab986621c4
pyakimova-sudo <pyakimova@gustavus.edu>
2026-04-07 12:43:24 -0500

    IDs added to custmozation page UI, compressed images & new prices to drinks

--- PITA_Rewards2/app/src/main/java/com/example/pita_rewards2/Drink_Menu.kt
+++ PITA_Rewards2/app/src/main/java/com/example/pita_rewards2/Drink_Menu.kt
@@ -14,37 +14,37 @@ data class Drink_Menu(
             Drink_Menu(
                 name = "Latte",
                 Drink_Type = "Coffee", //Will be used to sort might need another button
-                price = 2,
+                price = 5,
                 ingredients = listOf("Coffee", "Milk", "Ice")
             ),
             Drink_Menu(
                 name = "Smoothie",
                 Drink_Type = "Smoothie",
-                price = 1,
+                price = 3,
                 ingredients = listOf("")
             ),
             Drink_Menu(
                 name = "Matcha",
                 Drink_Type = "Matcha",
-                price = 1,
+                price = 5,
                 ingredients = listOf("Coffee", "Milk", "Ice")
             ),
             Drink_Menu(
                 name = "Cold Brew",
                 Drink_Type = "Coffee",
-                price = 1,
+                price = 5,
                 ingredients = listOf("Coffee", "Milk", "Ice")
             ),
             Drink_Menu(
                 name = "Water",
                 Drink_Type = "Regular",
-                price = 0,
+                price = 1,
                 ingredients = listOf("Water")
             ),
             Drink_Menu(
                 name = "Lemonade",
                 Drink_Type = "Lemonade",
-                price = 2,
+                price = 3,
                 ingredients = listOf("Ice")
             ),
             Drink_Menu(
@@ -56,7 +56,7 @@ data class Drink_Menu(
             Drink_Menu(
                 name = "Hot Chocolate",
                 Drink_Type = "Chocolate",
-                price = 86,
+                price = 5,
                 ingredients = listOf("Chocolate", "Milk")
             ),
             Drink_Menu(name = "Milk", Drink_Type = "Base?", price = 1, ingredients = listOf("Milk", "ice"))
